2b46bbb6f4f1c3b7739af26f55a450d2d720a1cd
Felipe Soares <felipesoareski@gmail.com>
2023-10-27 10:38:54 -0300

    correçao do exercico

--- exercicios/ex004a.py
+++ exercicios/ex004a.py
@@ -5,12 +5,5 @@ co = float(input('digite o comprimento do cateto oposto do treiangulo retangulo:
 ad = float(input('digite o comprimento do cateto adjacente: '))
 
 #calcule o quadrado de cada Cateto
-quadrado_a = co ** 2
-quadrado_b = ad ** 2
-
-#soma os quadrados dos catetos
-soma= quadrado_a + quadrado_b
-
-#calcule a raiz quadrada da soma
-Hipotenusa = math.sqrt(soma)
-print(f'o comprimento da hipotenusa é: {Hipotenusa:.2f}')
+hi = math.hypot(co, ad)
+print(f'o comprimento da hipotenusa é: {hi:.2f}')
