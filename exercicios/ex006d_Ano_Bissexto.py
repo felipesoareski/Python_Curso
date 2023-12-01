4e4e8485e7fff7684f6e21c234695f3be79594e3
Felipe Soares <felipesoareski@gmail.com>
2023-12-01 17:45:58 -0300

    versao corrigida-anoBissexto

--- exercicios/ex006d_Ano_Bissexto.py
+++ exercicios/ex006d_Ano_Bissexto.py
@@ -1,9 +1,9 @@
-ano = int(input('Digite um ano qualquer: '))
-if ano % 400 == 0:
-    print('é bissexto')
-elif ano % 100 == 0:
-    print('nao é Bissexto')
-elif ano % 4 == 0 :
-    print('é Bissixto')
-else: 
-    print('nao é bissexto')
+'''Faça um programa que leia um ano qualquer e mostre se ele é bissexto.'''
+from datetime import date
+
+ano = int(input('Qual ano você quer analizar?: '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 ==0:
+    ano = date.today().year 
+    print(f'O ano {ano} é BISSEXTO!')
+else:
+    print(f'O ano {ano} NÃO é BISSEXTO!')
