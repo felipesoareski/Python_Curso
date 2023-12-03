4a92b9346395d77ef71003ad239a30fca4ebe4a2
Felipe Soares <felipesoareski@gmail.com>
2023-12-03 15:01:30 -0300

    commentaria adicionado

--- exercicios/ex006f_Aumento_salar.py
+++ exercicios/ex006f_Aumento_salar.py
@@ -1,3 +1,6 @@
+'''Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. Para salários superiores a R$1250,00, 
+calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.'''
+
 salario = float(input('qual é o valor do seu salario? '))
 aumento1 = salario * (15/100)
 aumento2 = salario * (10/100)
@@ -6,6 +9,6 @@ if salario <= 1250:
     print(f'Seu aumento é de 15%, seu salario sera: R${aumento1+salario:.2f}')
 
 else:
-    print(f'Seu aumento é de 10%, seu salario sera: R${aumento2+salario:.2f}')
+    print(f'Seu aumento é de 10%, seu salário será: R${aumento2+salario:.2f}')
 
 print('---Parabens pelo aumento!---')
