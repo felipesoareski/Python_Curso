5659c44812b40ab2a896981dc21b826bab3f8dae
Felipe Soares <felipesoareski@gmail.com>
2023-12-03 15:13:33 -0300

    correçao do exercicio

--- exercicios/ex006g_Analise_Triangulo.py
+++ exercicios/ex006g_Analise_Triangulo.py
@@ -1,15 +1,10 @@
 print('----Vamos formar um triangulo!----')
-a = int(input('digite um numero '))
-b = int(input('digite outro um numero '))
-c = int(input('digite outro um numero '))
+a = float(input('Primeiro segmento '))
+b = float(input('Segundo segmento '))
+c = float(input('Terceiro numero '))
 
-if a < (b + c) :
-    print('Voce formou um triangulo!')
-
-elif b < (c + a):
-    print('Voce formou um triangulo!')
-elif c < (a + b):
-    print('Voce formou um triangulo!')
+if a < (b + c) and b < (c + a) and c < (a + b):
+    print('Voce FORMOU um triangulo!')
 else:
-    print('Infelizmente voce nao conseguiu, tente outra vez!')
+    print('Os seguimentos acima NÃO PODEM FORMAR triangulo!')
 
